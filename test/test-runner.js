17d04069156d43240530a328477ead7a7a1d0fca
Dev-0x0001 <dev@hno3.eu>
2026-02-24 11:45:26 +0100

    Update test/test-runner.js

---
+++ test/test-runner.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.default = void 0;
+
+var _mocha = require('mocha');
+
+function configureTestRunner() {
+  // Configure Mocha test runner
+  return _mocha;
+}
+
+var _default = configureTestRunner;
+exports.default = _default;
