d0ea0e6e2784f35c9f4082f43b71e52c6473e9f9
Dev-0x0001 <dev@hno3.eu>
2026-02-24 12:05:08 +0100

    Update test/unit/test_math_operations.py

---
+++ test/unit/test_math_operations.py
@@ -0,0 +1,16 @@
+# Unit Tests
+
+# This directory contains individual unit tests for components and functions
+
+# Example unit test
+import unittest
+
+class TestMathOperations(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(2 + 2, 4)
+
+    def test_subtraction(self):
+        self.assertEqual(5 - 3, 2)
+
+if __name__ == '__main__':
+    unittest.main()
